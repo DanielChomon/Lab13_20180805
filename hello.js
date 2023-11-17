7926c4ee129e148b0bb1aaff1911f539075a6734
Daniel <a20180805@pucp.edu.pe>
2023-11-16 21:56:08 -0500

    rezar

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
-console.log("hello");
+var cors = require('cors')
+const app = exp
+
+app.use(cors())
+
+
+app.get("/trabajadores/:dni", (req,res) => {
+    let dni = req.params.dni;
+
+    console.log("nombre: ", nombre , "| apellido: ", apellido, "| correo: ", correo);
+
+    let myJson = {
+        nombre : nombre,
+        apellido: apellido,
+        correo: correo,
+    };
+    res.json(myJson);
+});
