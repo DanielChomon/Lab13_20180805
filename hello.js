5438e9308abf1593aab73867ec9dfb14263df29c
Daniel <a20180805@pucp.edu.pe>
2023-11-16 22:00:41 -0500

    rezar 2

--- hello.js
+++ hello.js
@@ -3,7 +3,39 @@ const app = exp
 
 app.use(cors())
 
+pp.get("/trabajadores",(req, res) => {
+    let sql = "select * from trabajadores";
+    conn.query(sql,(err, result, fields) => {
+        if(err) throw err;
 
+        let horaActual = new Date();
+
+        for(let i = 0; i < result.length;i++ ){
+            result[i]["job_id"] += "_HOLAAAA"
+        }
+
+        res.json({
+            horaActual: horaActual,
+            trabajadores: result
+        });
+    });
+
+    pp.get("/sedes",(req, res) => {
+        let sql = "select * from sedes";
+        conn.query(sql,(err, result, fields) => {
+            if(err) throw err;
+
+            let horaActual = new Date();
+
+            for(let i = 0; i < result.length;i++ ){
+                result[i]["job_id"] += "_HOLAAAA"
+            }
+
+            res.json({
+                horaActual: horaActual,
+                sedes: result
+            });
+        });
 app.get("/trabajadores/:dni", (req,res) => {
     let dni = req.params.dni;
 
